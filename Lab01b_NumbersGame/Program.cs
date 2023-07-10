d1e12efc2bdcb7389bcbb7b44b43906d064e36d4
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-09 23:21:19 -0500

    populating

--- Lab01b_NumbersGame/Program.cs
+++ Lab01b_NumbersGame/Program.cs
@@ -34,10 +34,23 @@
             Console.WriteLine($"Product: {product}");
             Console.WriteLine($"Quotient: {quotient}");
         }
+        static int[] Populate(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number {i + 1}/{numbers.Length}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return numbers;
+        }
 
         static int GetSum(int[] numbers)
         {
-            Console.WriteLine();
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
         }
 
         static int GetProduct(int[] numbers, int sum)
